deec3b01820a7531143ea0466575c09fbdc42a7a
ajanieniolasolomon <eniolsolomon@gmail.com>
2018-10-19 16:20:43 +0100

    added travic cli

--- projects/ng-dialog-flow/dist/lib/ng-dialog-flow.constant.d.ts
+++
@@ -1,7 +0,0 @@
-export { SEARCH_NAME, SEARCH_PARAMETER, EXPECTED_RESULT, TOKEN };
-declare const SEARCH_NAME = "v";
-declare const SEARCH_PARAMETER = "20150910";
-declare const EXPECTED_RESULT = "application/json";
-declare const TOKEN: {
-    token: string;
-};
--- projects/ng-dialog-flow/dist/lib/ng-dialog-flow.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-export declare class NgDialogFlowService {
-    private config;
-    http: HttpClient;
-    constructor(config: any, http: HttpClient);
-    askQuestion(message: string, timezone?: string, sessionid?: string, lang?: string): any;
-}
